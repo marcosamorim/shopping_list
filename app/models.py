e09f705053d6b61fbc5a18912c400d98f6c56e82
Marcos Amorim <marcos.amorim@lockwood-publishing.com>
2020-09-06 20:20:27 +0100

    * WIP - Adding user and list models // Adding login to certain pages * few tweaks and typos

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
 
 class Product(db.Model):
@@ -9,4 +11,49 @@ class Product(db.Model):
     in_stock_quantity = db.Column(db.Integer())
 
     def __repr__(self):
-        return f"<Product {self.name}>"
+        return f"<Product {self.name} - {self.brand}>"
+
+
+@login.user_loader
+def load_user(uid):
+    return User.query.get(int(uid))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    gift_list_id = db.relationship("GiftList", backref="gift_list", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+gifts = db.Table(
+    "gifts",
+    db.Column("product_id", db.Integer, db.ForeignKey("product.id"), primary_key=True),
+    db.Column(
+        "gift_list_id", db.Integer, db.ForeignKey("gift_list.id"), primary_key=True
+    ),
+    db.Column("gift_purchased", db.Boolean, default=False),
+)
+
+
+class GiftList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    products = db.relationship(
+        "Product",
+        secondary=gifts,
+        lazy="subquery",
+        backref=db.backref("gift_list", lazy=True),
+    )
+
+    def __repr__(self):
+        return f"<GiftList {self.user_id}"
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template, flash, redirect
+# from flask import render_template, flash, redirect
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    make_response,
+)
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
 from app import app
 from app.forms import LoginForm
-from app.models import Product
+from app.models import Product, User
 
 
 @app.route("/")
@@ -11,22 +23,56 @@ def index():
     return render_template("index.html", title=title)
 
 
-@app.route("/list")
-def product_list():
-    title = "Product List"
-    products = Product.query.all()
-    return render_template("list.html", title=title, products=products)
-
-
+# Login routes
+# ---------------------------------------
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user {form.username.data}")
-        return redirect("/index")
+        # validate user login attempt
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+
+        # if no next argument is specified, redirect to index
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+
     return render_template("login.html", title="Sign In", form=form)
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+# Product related routes
+# ------------------------------------
+@login_required
+@app.route("/list")
+def product_list():
+    title = "Product List"
+    cookied_list = request.cookies.get("products")
+    if cookied_list:
+        products_on_list = Product.query.filter(id__in=cookied_list.split(","))
+    else:
+        products_on_list = []
+
+    products = Product.query.all()
+    return render_template(
+        "list.html", title=title, products=products, products_on_list=products_on_list
+    )
+
+
+@login_required
 @app.route("/gift")
 def gift_list():
     pass
